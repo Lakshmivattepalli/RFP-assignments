31d6c203cf8fb2c4d0c5b051b6c366f2b0b5a112
srikala <vlslaksh1899@gmail.com>
2022-03-14 02:02:07 +0530

    modified

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
 
 namespace ConsoleApp1
 {
@@ -14,6 +19,10 @@ namespace ConsoleApp1
         {
             int is_Full_Time = 1;
             int is_Part_Time = 2;
+            int maxRatePerMonth = 100;
+            int empRatePerHr = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
               Random obj = new Random();
 
@@ -30,7 +39,25 @@ namespace ConsoleApp1
                 }
             }
 
+        
+        public void DailyWages()
+        {
+                int empCheck = obj.Next(0, 2);
+             if (empCheck == is_Full_Time)
+            {
+                empHrs = 8;
+                Console.WriteLine("Employee worked full day");
+            }
+            else
+            {
+                empHrs = 0;
+                Console.WriteLine("Employee is absent");
+            }
+            empWage = empHrs * empRatePerHr;
+            Console.WriteLine("The salary of an Employee is: " + empWage);
+        }
+        }
+
         }
     }
-}
-
+      
