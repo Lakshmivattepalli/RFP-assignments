78beb8c3480bc637746ba726bda3ab5156aba747
srikala <vlslaksh1899@gmail.com>
2022-03-14 01:01:33 +0530

    modified the files

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Welcome to the Employee Wage Computations Program");
+            Console.ReadLine();
         }
     }
 }
