6d7aa6147d04aa5e3a5e1a7f9ae679a0448e0976
srikala <vlslaksh1899@gmail.com>
2022-03-14 01:07:38 +0530

    added

--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
 <<<<<<< HEAD
+<<<<<<< HEAD
             Console.WriteLine("Hello, Welcome to the Employee Wage Computations Program");
+=======
+            Console.WriteLine("Hello! Welcome to Employee Wage Computation Program");
+>>>>>>> Hello
 =======
             Console.WriteLine("Hello! Welcome to Employee Wage Computation Program");
 >>>>>>> Hello
