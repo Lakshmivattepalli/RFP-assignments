58f20ecbf6cfedf017996fb6527e04f4afaea54b
srikala <vlslaksh1899@gmail.com>
2022-03-14 12:17:15 +0530

    modified program

--- Program.cs
+++ Program.cs
@@ -15,8 +15,10 @@ namespace ConsoleApp1
             Console.WriteLine("Hello, Welcome to the Employee Wage Computations Program");
             Console.ReadLine();
         }
-        internal class Class1
-        {
+
+
+       internal class Class1
+       {
             int is_Full_Time = 1;
             int is_Part_Time = 2;
             int maxRatePerMonth = 100;
@@ -24,7 +26,7 @@ namespace ConsoleApp1
             int empHrs = 0;
             int empWage = 0;
 
-              Random obj = new Random();
+            Random obj = new Random();
 
             public void Employee()
             {
@@ -39,25 +41,44 @@ namespace ConsoleApp1
                 }
             }
 
-        
-        public void DailyWages()
-        {
-                int empCheck = obj.Next(0, 2);
-             if (empCheck == is_Full_Time)
-            {
-                empHrs = 8;
-                Console.WriteLine("Employee worked full day");
-            }
-            else
-            {
-                empHrs = 0;
-                Console.WriteLine("Employee is absent");
-            }
-            empWage = empHrs * empRatePerHr;
-            Console.WriteLine("The salary of an Employee is: " + empWage);
-        }
-        }
 
-        }
+            public void DailyWages()
+            {
+                int empCheck = obj.Next(0, 2);
+                if (empCheck == is_Full_Time)
+                {
+                    empHrs = 8;
+                    Console.WriteLine("Employee worked full day");
+                }
+                else
+                {
+                    empHrs = 0;
+                    Console.WriteLine("Employee is absent");
+                }
+                empWage = empHrs * empRatePerHr;
+                Console.WriteLine("The salary of an Employee is: " + empWage);
+            }
+            public void EmpPartTime()
+            {
+                int randomCheck = obj.Next(0, 3);
+                if (randomCheck == is_Full_Time)
+                {
+                    empHrs = 8;
+                    Console.WriteLine("Employee has worked full day");
+                }
+                else if (randomCheck == is_Part_Time)
+                {
+                    empHrs = 4;
+                    Console.WriteLine("Employee has worked half day");
+                }
+                empWage = empHrs * empRatePerHr;
+                Console.WriteLine(" The salary of this Employee is:" + empWage );
+            }
+
+       }
+
+
+       
     }
+}
       
